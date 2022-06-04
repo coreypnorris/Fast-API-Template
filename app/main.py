66d4c146fcc8783988d214a3cc86aa06c0ee66df
Corey Norris <coreypnorris@gmail.com>
2022-06-04 18:56:21 -0400

    Add sample data & changes to enable VS Code debug

--- app/main.py
+++ app/main.py
@@ -1,10 +1,43 @@
-from fastapi import FastAPI
+from typing import List, Optional
+from fastapi import FastAPI, HTTPException
+import uvicorn
 
 
 app = FastAPI(title="Fast API Template")
 
+db = [
+    {"id": 1, "size": "s", "fuel": "gasoline", "doors": 3, "transmission": "auto"},
+    {"id": 2, "size": "s", "fuel": "electric", "doors": 3, "transmission": "auto"},
+    {"id": 3, "size": "s", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
+    {"id": 4, "size": "m", "fuel": "electric", "doors": 3, "transmission": "auto"},
+    {"id": 5, "size": "m", "fuel": "hybrid", "doors": 5, "transmission": "auto"},
+    {"id": 6, "size": "m", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
+    {"id": 7, "size": "l", "fuel": "diesel", "doors": 5, "transmission": "manual"},
+    {"id": 8, "size": "l", "fuel": "electric", "doors": 5, "transmission": "auto"},
+    {"id": 9, "size": "l", "fuel": "hybrid", "doors": 5, "transmission": "auto"}
+]
 
-@app.get("/")
-def welcome():
-    """Return a friendly welcome message."""
-    return {'message': "Welcome to the Car Sharing service!"}
+
+@app.get("/api/cars")
+def get_cars(size: Optional[str] = None, doors: Optional[int] = None) -> List:
+    result = db
+    if size:
+        result = [car for car in result if car['size'] == size]
+    if doors:
+        result = [car for car in result if car['doors'] >= doors]
+    return result
+
+
+@app.get("/api/cars/{id}")
+def car_by_id(id: int) -> dict:
+    result = [car for car in db if car['id'] == id]
+    if result:
+        return result[0]
+    else:
+        raise HTTPException(status_code=404, detail=f"No car with id={id}.")
+
+
+# This is required for IDE debugging.
+# Also need to modify the launch.json for VS Code debugger to work per https://stackoverflow.com/a/63271966.
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", reload=True)
