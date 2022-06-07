8e5ac8b5f059a89120586d9278038b28c0bf228e
Corey Norris <coreypnorris@gmail.com>
2022-06-07 10:38:48 -0400

    Add router to support html templates

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
-from app.routers import cars
+from app.routers import cars, web
 
 app = FastAPI(title="Fast API Template")
 app.include_router(cars.router)
+app.include_router(web.router)
 
 
 # This is required for VS Code IDE debugging.
