fd3b2f19d1ffe8c52a40bc03135a8b725ff0d766
Corey Norris <coreypnorris@gmail.com>
2022-06-12 12:47:56 -0700

    Add cors middleware

--- app/main.py
+++ app/main.py
@@ -1,13 +1,31 @@
 import uvicorn
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import cars, web
 from starlette.responses import JSONResponse
 from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY, HTTP_500_INTERNAL_SERVER_ERROR
 from app.routers.cars import BadTripException
 
+
+# Routers
 app = FastAPI(title="Fast API Template")
 app.include_router(cars.router)
 app.include_router(web.router)
+
+
+# Middleware
+origins = [
+    "http://localhost:8000"
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.middleware("http")
@@ -23,6 +41,7 @@ async def global_exception_handler_middleware(request: Request, call_next):
         )
 
 
+# Custom exception handlers
 @app.exception_handler(BadTripException)
 async def unicorn_exception_handler(request: Request, exc: BadTripException):
     return JSONResponse(
