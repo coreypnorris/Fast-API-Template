3821cae135a63d9e79fba7ddcbc9fa73a6fe75b6
Corey Norris <coreypnorris@gmail.com>
2022-06-12 12:32:31 -0700

    add middleware for global exception handling

--- app/main.py
+++ app/main.py
@@ -2,12 +2,26 @@ import uvicorn
 from fastapi import FastAPI, Request
 from app.routers import cars, web
 from starlette.responses import JSONResponse
-from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
+from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY, HTTP_500_INTERNAL_SERVER_ERROR
 from app.routers.cars import BadTripException
 
 app = FastAPI(title="Fast API Template")
 app.include_router(cars.router)
 app.include_router(web.router)
+
+
+@app.middleware("http")
+async def global_exception_handler_middleware(request: Request, call_next):
+    try:
+        return await call_next(request)
+    except Exception as ex:
+        # Add custom logging here in future. For now, just print to console.
+        print(str(ex))
+        return JSONResponse(
+            status_code=HTTP_500_INTERNAL_SERVER_ERROR,
+            content={"message": "Internal server error."},
+        )
+
 
 @app.exception_handler(BadTripException)
 async def unicorn_exception_handler(request: Request, exc: BadTripException):
@@ -15,6 +29,7 @@ async def unicorn_exception_handler(request: Request, exc: BadTripException):
         status_code=HTTP_422_UNPROCESSABLE_ENTITY,
         content={"message": "Bad Trip"},
     )
+
 
 # This is required for VS Code IDE debugging.
 # Also need to modify the launch.json for VS Code debugger to work per https://stackoverflow.com/a/63271966.
